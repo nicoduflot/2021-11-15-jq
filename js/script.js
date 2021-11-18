ac3ef06ecd7b3de11d659ba17181d2e8e88ba6d5
Nicolas Duflot <nduflot@jehann.fr>
2021-11-18 11:50:58 +0100

    javascript cookie

--- js/script.js
+++ js/script.js
@@ -28,3 +28,26 @@ function loadNav(selector){
         console.log(error.message);
     });
 }
+
+// fonctions de gestions des cookies
+function setCookie(name, value ='', day=-1){
+    let dateNow = new Date();
+    dateNow.setTime( dateNow.getTime() + ( day * 24 * 60 * 60 * 1000));
+    document.cookie = `${name}=${value};  expires=${dateNow}; path=/; SameSite=Strict`;
+}
+
+// récupérer un cookie
+
+function getCookie(name){
+    /*
+    unCookie=23; autrecookie=12;     
+    */
+   let tabCookie = document.cookie.split('; ');
+   for(cookie of tabCookie){
+       let tabValue = cookie.split('=');
+       if(name === tabValue[0]){
+           return tabValue[1];
+       }
+   }
+   return false;
+}
